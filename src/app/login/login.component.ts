7944758c03906aaec7d6aa199bd65b3f001bf1b6
Sudheesh <ohyezsudhi@gmail.com>
2022-04-01 13:28:38 +0530

    One way binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  msg = "Your Perfect Banking Partner"   //String Interpolation syntx: {{variable-name}} in html page
+  acc = "account number please"  //property binding example: [placeholder]="acc"
+  accNo = ""
+  pwd = ""
+
+
+  database = {
+    1000: { acno: 1000, uname: "abin", password: 1000, balance: 5000 },
+    1001: { acno: 1001, uname: "anu", password: 1001, balance: 4000 },
+    1002: { acno: 1002, uname: "jith", password: 1002, balance: 3000 },
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  acnoChange(event: any) {
+    this.accNo = event.target.value
+    // console.log(this.accNo);
 
+  }
+  pwdChange(event: any) {
+    this.pwd = event.target.value
+    // console.log(this.pwd);
+
+  }
+
+
+  login() {
+    alert("login clicked")
+  }
 }
