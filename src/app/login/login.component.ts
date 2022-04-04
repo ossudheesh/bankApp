3c8f86b6ad3434f2216d47820e453c872f231ad1
Sudheesh <ohyezsudhi@gmail.com>
2022-04-04 11:06:52 +0530

    event handling using

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   pwd = ""
 
 
-  database = {
+  database:any = {
     1000: { acno: 1000, uname: "abin", password: 1000, balance: 5000 },
     1001: { acno: 1001, uname: "anu", password: 1001, balance: 4000 },
     1002: { acno: 1002, uname: "jith", password: 1002, balance: 3000 },
@@ -35,6 +35,20 @@ export class LoginComponent implements OnInit {
 
 
   login() {
-    alert("login clicked")
+    let acno = this.accNo
+    let pwd= this.pwd
+    let db=this.database
+
+    if(acno in db){
+      if(pwd==db[acno]["password"]){
+        alert('Login Successfull')
+      }
+      else{
+        alert('Password Error')
+      }
+    }
+    else{
+      alert('Invalid User')
+    }
   }
 }
